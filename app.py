ad7e3e777705560a782a50f8cac16153b04e6303
Srbaqua <23bcs101@nith.ac.in>
2025-07-21 21:58:40 +0530

    port changed

--- app.py
+++ app.py
@@ -84,4 +84,5 @@ def stream_scan(competitor_id):
     return Response(generate(), mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5002)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
